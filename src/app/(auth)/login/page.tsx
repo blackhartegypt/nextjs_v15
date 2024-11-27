0758149e76ab2f42f6fbf0b5b2cebe0da78cc3e1
blackhartegypt <blackhartegypt@gmail.com>
2024-11-27 09:49:31 +0300

    group routing

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login(){
+    return(
+        <>
+            <h2>Login Page</h2>
+            <h2>This is group routing you wont have to specify auth in the url because they are grouped together</h2>
+        </>
+        
+    )
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Register(){
+    return(
+        <>
+            <h2>Register Page and Component</h2>
+            <h2>you wont have to specify auth in the url</h2>
+        </>
+    )
+}
