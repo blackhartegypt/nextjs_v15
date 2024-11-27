7d72affd1930ad71b3d9f8fa054240629ba91439
blackhartegypt <blackhartegypt@gmail.com>
2024-11-27 11:24:02 +0300

    automatic navigation using useRouter from next/navigation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,14 @@
-
+"use client"
+import {useRouter} from "next/navigation";
 
 export default function About(){
+    const router=useRouter();
+
     return(
         <>
 
             <h2>This is the basic file Routing In the About Page</h2>
+            <button onClick={()=>router.push("/")} className="bg-blue-500 text-white p-2 rounded-md">Go Home</button>
         </>
         
 
