bb08c9a0e052b33ffb9e2a935397dc2dda214176
blackhartegypt <blackhartegypt@gmail.com>
2024-11-27 08:47:30 +0300

    basic server component

---
+++ src/app/components/greet.tsx
@@ -0,0 +1,11 @@
+
+export const Greet=()=>{
+
+    return(
+        <>
+        <h2>This is the an example of a server component</h2>
+        <h3>It is always rendred exclusively on the server</h3>
+        </>
+        
+    )
+}
