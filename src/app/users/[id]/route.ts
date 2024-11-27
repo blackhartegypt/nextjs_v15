bdfbb78f87dbdce150fe6ce3ed029e3606076276
blackhartegypt <blackhartegypt@gmail.com>
2024-11-27 12:39:35 +0300

    Dynamic Route handling

---
+++ src/app/users/[id]/route.ts
@@ -0,0 +1,30 @@
+import {users} from "../route";
+
+export async function GET(_request:Request,{params}:{params:{id:string}}){
+    const {id}=await params;
+    const user=users.find((user)=>user.id === parseInt(id));
+    return Response.json(user)
+}
+
+export async function DELETE(request:Request,{params}:{params:{id:string}}){
+    const {id}=await params;
+    const userIndex=users.findIndex((user)=>user.id === parseInt(id));
+
+    if(userIndex === -1){
+        return new Response(JSON.stringify({message:"user not found"}),{
+            headers:{
+                "Content-Type":"application/json"
+            },
+            status:404,
+        })
+    }
+
+    const deletedUser=users.splice(userIndex,1);
+
+    return new Response(JSON.stringify({message:"user deleted", user: deletedUser[0]}),{
+        headers:{
+            "Content-Type":"application/json"
+        },
+        status:200,
+    })
+}
