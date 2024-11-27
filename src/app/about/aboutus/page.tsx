5ec73fd044d9a6922389677cc4cac2a754401ed9
blackhartegypt <blackhartegypt@gmail.com>
2024-11-27 09:11:03 +0300

    basic and nested file routing

---
+++ src/app/about/aboutus/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function AboutUs(){
+    return(
+        <h2>This is the about us page that shows the nested file routing by visiting /about/aboutus</h2>
+    )
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function About(){
+    return(
+        <>
+
+            <h2>This is the basic file Routing In the About Page</h2>
+        </>
+        
+
+    )
+}
